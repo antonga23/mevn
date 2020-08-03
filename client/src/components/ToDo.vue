3efd7be3769428e40b8abfbad8fb5be4025f8e98
alatha <alathantonga@gmail.com>
2020-08-03 19:15:01 +0200

    routes working

---
+++ client/src/components/ToDo.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>
+    <ul>
+      <li>
+        Hello World
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ToDo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style lang="css">
+</style>
